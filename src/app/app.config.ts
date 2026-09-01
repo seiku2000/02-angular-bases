a5ffa0b2188348b73c51b793a54bd0193a02996c
seiku2000 <eslumter@gmail.com>
2026-09-01 11:52:46 -0700

    add hashFindServer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,18 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    //el hash es para que funcione en cualquier servidor que no tenga configuracion
+    //el hash # indica que es un fragmento de la pagina y no se envia al servidor
+    //es decir que el servidor no sabe de la existencia de la ruta
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+    },
   ]
 };
